5f2aae93970c0d7232e32f8cbd0ac360f19b946e
Ajay kumar <ajcoder25@gmail.com>
2025-05-20 21:29:56 +0530

    20th may progress

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,9 +1,44 @@
-'use client'
+"use client"
+import { supabase } from '@/services/supabaseClient'
+import React, {useState} from 'react'
 
-function ({children}){
-  return(
-     <div>
-        supabase.auth.getUser().then(async({data,error}))
-     </div>
-  )
+function Provider({children}){
+  const [user, setUser] = useState()
+
+  useEffect(()=>{
+    CreateNewUser()
+
+  },[])
+
+    const CreateNewUser = ()=>{
+
+      supabase.auth.getUser().then(async({data:{user}}) =>{
+         //Check if user already exists
+         let {data:Users, error} = await supabase
+          .from('Users')
+          .select("*")
+          .eq('email', user?.email)
+
+          console.log(Users)
+      //if not then create new user
+
+      if(Users?.length ==0){
+       const{data, error} = await supabase.from("Users")
+        .insert([
+          {
+           name:user?.user_metadata?.name,
+           email:user?.email,
+           picture:user?.user_metadata?.picture
+
+        }
+        ])
+        console.log(data)
+        setUser(data)
+        return
+      }
+      setUser(Users[0])
+
+      })
+     
+    }
 }
