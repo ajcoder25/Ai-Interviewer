c978d35ef6af795eb8489903b66f0949b4b7414a
Ajay kumar <ajcoder25@gmail.com>
2025-05-21 21:06:05 +0530

    updated the usercontext

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,6 +1,7 @@
 "use client"
+import { UserDetailContext } from '@/context/UserDetailContext'
 import { supabase } from '@/services/supabaseClient'
-import React, {useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 
 function Provider({children}){
   const [user, setUser] = useState()
@@ -15,7 +16,7 @@ function Provider({children}){
       supabase.auth.getUser().then(async({data:{user}}) =>{
          //Check if user already exists
          let {data:Users, error} = await supabase
-          .from('Users')
+          .from('users')
           .select("*")
           .eq('email', user?.email)
 
@@ -23,7 +24,7 @@ function Provider({children}){
       //if not then create new user
 
       if(Users?.length ==0){
-       const{data, error} = await supabase.from("Users")
+       const{data, error} = await supabase.from("users")
         .insert([
           {
            name:user?.user_metadata?.name,
@@ -41,4 +42,20 @@ function Provider({children}){
       })
      
     }
+
+    return(
+           <UserDetailContext.Provider value = {{user, setUser}}>
+            <div>
+            {children}
+            </div>
+           </UserDetailContext.Provider>
+    )
+}
+
+
+export default Provider
+
+export const useUser = ()=>{
+  const context = useContext(UserDetailContext)
+  return context
 }
--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -4,7 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log("supabaseUrl", supabaseUrl);
-console.log("supabaseAnonKey", supabaseAnonKey);
 // you need to export it so you can use supabase anywhere
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
